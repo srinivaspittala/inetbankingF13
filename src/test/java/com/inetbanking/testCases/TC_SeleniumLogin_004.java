d8b2af7fa7d733329d1d055e53e682be002564a3
srinivasunites <srinivas.unites@gmail.com>
2018-12-30 17:14:22 +0530

    This is My first Framework commit

---
+++ src/test/java/com/inetbanking/testCases/TC_SeleniumLogin_004.java
@@ -0,0 +1,90 @@
+package com.inetbanking.testCases;
+import java.io.IOException;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import com.inetbanking.pageObjects.Seleniumloginpage;
+import com.inetbanking.pageObjects.LoginPage;
+
+
+public class TC_SeleniumLogin_004 extends BaseClass
+
+{
+	@Test
+	public void seleniumtologin() throws InterruptedException, IOException
+	{
+		logger.info("URL is opened");
+		
+		LoginPage lp=new LoginPage(driver);
+		
+		lp.setUserName(username);
+		logger.info("User name is provided");
+		
+		lp.setPassword(password);
+		logger.info("Passsword is provided");
+		
+		lp.clickSubmit();
+		logger.info("Clicked on Submit Button");
+		
+		Thread.sleep(3000);
+	
+		Seleniumloginpage slogp=new Seleniumloginpage(driver);
+		
+		logger.info("Checking Selenium to Login");
+		
+		slogp.clickselenium();
+		
+		slogp.clicklogin();
+		
+		slogp.setemail("srinivas.motoe@gmail.com");
+		
+		slogp.setPassword("chaitra404");
+		
+		slogp.clicksignin();
+				
+		
+		Thread.sleep(3000);
+		
+//		logger.info("validation started....");
+//		
+//		boolean res=driver.getPageSource().contains("Successfully Logged in...");
+//		
+//		if(res==true)
+//		{
+//			Assert.assertTrue(true);
+//			logger.info("test case passed....");
+//			
+//		}
+//		else
+//		{
+//			logger.info("test case failed....");
+//			captureScreen(driver,"seleniumtologin");
+//			Assert.assertTrue(false);
+//		}
+			
+//		if(driver.getTitle().equals("Successfully Logged in..."))
+//		{
+//			Assert.assertTrue(true);
+//			logger.info("Login test passed");
+//		}
+//		else
+//		{
+//			captureScreen(driver,"loginTest");
+//			Assert.assertTrue(false);
+//			logger.info("Login test failed");
+//		}	
+	
+		if(driver.getPageSource().contains("Successfully Logged in..."))
+		{
+			Assert.assertTrue(true);
+			logger.info("Login test passed");
+		}
+		else
+		{
+			captureScreen(driver,"loginTest");
+			Assert.assertTrue(false);
+			logger.info("Login test failed");
+		}	
+		
+	}	
+	
+}
